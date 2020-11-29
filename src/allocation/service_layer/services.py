cosmicpython/code

use uow in services, flask app

src/allocation/services.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from allocation import model
 from allocation.model import OrderLine
-from allocation.repository import BatchRepository
+
 
 class InvalidSku(Exception):
     pass
@@ -11,11 +11,11 @@ class InvalidSku(Exception):
 def is_valid_sku(sku, batches):
     return sku in {b.sku for b in batches}
 
-def allocate(line: OrderLine, repo: BatchRepository, session) -> str:
-    batches = repo.list()
-    if not is_valid_sku(line.sku, batches):
-        raise InvalidSku(f'Invalid sku {line.sku}')
-    batch = model.allocate(line, batches)
-    session.commit()
+def allocate(line: OrderLine, start_uow) -> str:
+    with start_uow() as uow:
+        batches = uow.batches.list()
+        if not is_valid_sku(line.sku, batches):
+            raise InvalidSku(f'Invalid sku {line.sku}')
+        batch = model.allocate(line, batches)
+        uow.commit()
     return batch
-

SuiyunCloud/Python-DDD

src/allocation/service_layer/services.py
@@ -4,7 +4,8 @@ from datetime import date
 
 from allocation.domain import model
 from allocation.domain.model import OrderLine
-from allocation.adapters.repository import AbstractRepository
+from allocation.service_layer import unit_of_work
+
 
 class InvalidSku(Exception):
     pass
@@ -16,19 +17,22 @@ def is_valid_sku(sku, batches):
 
 def add_batch(
         ref: str, sku: str, qty: int, eta: Optional[date],
-        repo: AbstractRepository, session,
+        uow: unit_of_work.AbstractUnitOfWork
 ):
-    repo.add(model.Batch(ref, sku, qty, eta))
-    session.commit()
+    with uow:
+        uow.batches.add(model.Batch(ref, sku, qty, eta))
+        uow.commit()
 
 
 def allocate(
-        orderid: str, sku: str, qty: int, repo: AbstractRepository, session
+        orderid: str, sku: str, qty: int,
+        uow: unit_of_work.AbstractUnitOfWork
 ) -> str:
     line = OrderLine(orderid, sku, qty)
-    batches = repo.list()
-    if not is_valid_sku(line.sku, batches):
-        raise InvalidSku(f'Invalid sku {line.sku}')
-    batchref = model.allocate(line, batches)
-    session.commit()
+    with uow:
+        batches = uow.batches.list()
+        if not is_valid_sku(line.sku, batches):
+            raise InvalidSku(f'Invalid sku {line.sku}')
+        batchref = model.allocate(line, batches)
+        uow.commit()
     return batchref