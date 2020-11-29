cosmicpython/code

first cut of flask app [first_cut_flask_app]

flask_app.py
@@ -1,0 +1,27 @@
+from flask import Flask, jsonify, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import config
+import model
+import orm
+import repository
+
+
+orm.start_mappers()
+get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
+app = Flask(__name__)
+
+@app.route("/allocate", methods=['POST'])
+def allocate_endpoint():
+    session = get_session()
+    batches = repository.BatchRepository(session).list()
+    line = model.OrderLine(
+        request.json['orderid'],
+        request.json['sku'],
+        request.json['qty'],
+    )
+
+    batchid = model.allocate(line, batches)
+
+    return jsonify({'batchid': batchid}), 201

SuiyunCloud/Python-DDD

flask_app.py
@@ -1,0 +1,27 @@
+from flask import Flask, jsonify, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import config
+import model
+import orm
+import repository
+
+
+orm.start_mappers()
+get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
+app = Flask(__name__)
+
+@app.route("/allocate", methods=['POST'])
+def allocate_endpoint():
+    session = get_session()
+    batches = repository.SqlAlchemyRepository(session).list()
+    line = model.OrderLine(
+        request.json['orderid'],
+        request.json['sku'],
+        request.json['qty'],
+    )
+
+    batchref = model.allocate(line, batches)
+
+    return jsonify({'batchref': batchref}), 201