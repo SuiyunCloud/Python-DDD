cosmicpython/code

flask now does error handling [flask_error_handling]

flask_app.py
@@ -12,6 +12,9 @@ orm.start_mappers()
 get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 app = Flask(__name__)
 
+def is_valid_sku(sku, batches):
+    return sku in {b.sku for b in batches}
+
 @app.route("/allocate", methods=['POST'])
 def allocate_endpoint():
     session = get_session()
@@ -22,7 +25,14 @@ def allocate_endpoint():
         request.json['qty'],
     )
 
-    batchid = model.allocate(line, batches)
+    if not is_valid_sku(line.sku, batches):
+        return jsonify({'message': f'Invalid sku {line.sku}'}), 400
+
+    try:
+        batchid = model.allocate(line, batches)
+    except model.OutOfStock as e:
+        return jsonify({'message': str(e)}), 400
 
     session.commit()
     return jsonify({'batchid': batchid}), 201
+

SuiyunCloud/Python-DDD

flask_app.py
@@ -12,6 +12,9 @@ orm.start_mappers()
 get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 app = Flask(__name__)
 
+def is_valid_sku(sku, batches):
+    return sku in {b.sku for b in batches}
+
 @app.route("/allocate", methods=['POST'])
 def allocate_endpoint():
     session = get_session()
@@ -22,7 +25,14 @@ def allocate_endpoint():
         request.json['qty'],
     )
 
-    batchref = model.allocate(line, batches)
+    if not is_valid_sku(line.sku, batches):
+        return jsonify({'message': f'Invalid sku {line.sku}'}), 400
+
+    try:
+        batchref = model.allocate(line, batches)
+    except model.OutOfStock as e:
+        return jsonify({'message': str(e)}), 400
 
     session.commit()
     return jsonify({'batchref': batchref}), 201
+